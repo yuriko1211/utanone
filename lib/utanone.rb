9698433097532aa54b24d48f99ffb9894c2ba121
yuko yuri <yuriko11d@gmail.com>
2021-09-23 16:10:10 +0900

    フォルダ階層を変更

--- lib/utanone.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-require_relative "utanone/version"
-require_relative "utanone/uta"
-module Utanone
-  class Error < StandardError; end
-  # Your code goes here...
-end
---
+++ lib/utanone/utanone.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "version"
+require_relative "uta"
+module Utanone
+  class ParseError < StandardError; end
+  # Your code goes here...
+end
